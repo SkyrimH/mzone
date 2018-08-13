1c2ea4c5775bb74d7e58c7ed1eabe0868bfbfc70
SkyrimH <huang.shan12@qq.com>
2018-08-14 00:56:35 +0800

    修正

--- src/pages/ebookpage/ebookpage.vue
+++ src/pages/ebookpage/ebookpage.vue
@@ -54,7 +54,7 @@
             getEbookInfoSucc(res) {
                 if (res.status == 200) {
                     this.bookList = res.data
-                    console.log(this.bookList);
+                    // console.log(this.bookList);
                 }
             }
         },
